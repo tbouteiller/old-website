af86d26837f5fa6fc49aa20498acb278e5384f6a
tbouteiller <56732647+tbouteiller@users.noreply.github.com>
2021-04-08 09:22:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+//PAGE TRANSITION FUNCTION
+function pageTransition() {
+  const anchors = document.querySelectorAll("a");
+  const transition_el = document.querySelector(".transition");
+
+  setTimeout(() => {
+    transition_el.classList.remove("is-active");
+  }, 300);
+}
+
 //NAV BAR FUNCTIONALITY
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  //document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  //document.getElementById("main").style.marginLeft = "0";
 }
 
-window.onload = function () {
+//SCROLL FUNCTION
+function scrollEffect() {
   const REACT = document.querySelector(".react");
   const NODE = document.querySelector(".node");
   const SASS = document.querySelector(".sass");
@@ -43,4 +54,32 @@ window.onload = function () {
       SASS.style.transform = "translateX(-500px)";
     }
   }
-};
+}
+
+//FORM VALIDATION
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
